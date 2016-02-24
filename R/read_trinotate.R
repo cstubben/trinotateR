da0a72d3d89e11cbe1718ccef71fce7cd7c7c578
Chris Stubben <chris.stubben@gmail.com>
2016-02-24 08:51:23 -0700

    update read_trinotate

--- R/read_trinotate.R
+++ R/read_trinotate.R
@@ -1,6 +1,6 @@
-read_trinotate <- function(file){
+read_trinotate <- function(file, ...){
    ## suppress fread warning messages like Bumped column 5 to type character 
-   x <- suppressWarnings( fread(file, sep="\t", na.strings=".") )
+   x <- suppressWarnings( fread(file, sep="\t", na.strings=".", ...) )
    # message("Read ", nrow(x), " rows")
    # replace "#gene_id"  since names in row 1 start with # 
    names(x)[1] <- "gene_id"
